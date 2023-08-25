cbffed46a55b0ddb7457ba02787929a3e3d36ec1
loki-pepe <lovroperaic@gmail.com>
2023-08-25 15:42:31 +0200

    edit create listing page, add widgets to ListingForm

--- auctions/forms.py
+++ auctions/forms.py
@@ -5,7 +5,8 @@ from .models import User, Listing, Bid, Comment
 
 
 class ListingForm(ModelForm):
-    start_price = forms.DecimalField(max_digits=12, decimal_places=2, min_value=0)
+    start_price = forms.DecimalField(max_digits=12, decimal_places=2, min_value=0, widget=forms.TextInput(attrs={'placeholder': '$'}))
+    description = forms.CharField(widget=forms.Textarea)
 
     class Meta:
         model = Listing
